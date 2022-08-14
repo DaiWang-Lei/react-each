f0e3f55a28fd04ef476d2339edf22919de2f0c09
DaiWang <1403693160@qq.com>
2022-08-14 22:39:13 +0800

    feat: init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Button } from "antd";
+import TButton from "./components/TButton";
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <TButton>123</TButton>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TButton.jsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd'
+import React, { Component } from 'react'
+
+export default class TButton extends Component {
+  render() {
+    return (
+      <Button>Button</Button>
+    )
+  }
+}
