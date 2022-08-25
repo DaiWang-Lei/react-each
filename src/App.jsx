11fdc335bb5c82a05ac65c3c8c2e3574f8b03e73
贾康杰 <1403693160@qq.com>
2022-08-25 20:39:45 +0800

    feat: 新增table demo 验证

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,35 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Button } from "antd";
 import TButton from "./components/TButton";
+import ATable from "./components/ATable";
 
-
-
+const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
 function App() {
-
   return (
     <div className="App">
-      <TButton>123</TButton>
+      <ATable data={data} />
     </div>
   );
 }
